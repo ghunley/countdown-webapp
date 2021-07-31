2c123260f092680dca4524c2af659810dd7b1173
ghunley <ghunley.zhang@gmail.com>
2021-07-30 20:32:40 -0700

    Resize background based on window size

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ import './style/style.css'
 
 function App() {
 
+    const updateWidthAndHeight = () => {
+      setWindowWidth(window.innerWidth);
+      setWindowHeight(window.innerHeight);
+    };
+
+    React.useEffect(() => {
+        window.addEventListener("resize", updateWidthAndHeight);
+        return () => window.removeEventListener("resize", updateWidthAndHeight);
+    });
+
+    const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+    const [windowHeight, setWindowHeight] = React.useState(window.innerHeight);
+
     // Random component
     const Completionist = () => <span>You are good to go!</span>;
 
@@ -34,16 +47,36 @@ function App() {
         );
         }
     };
+    getBgStyle();
 
-  return (
-      <div className="background">
-        <div class="message">Yuxiao's flight to Seattle is departing in</div>
-        <Countdown
-            date={1629240900000}
-            renderer={renderer}
-        />
-      </div>
-  );
+    return (
+        <div className="background" style={getBgStyle()}>
+            <div class="message">Yuxiao's flight to Seattle is departing in</div>
+            <Countdown
+                date={1629240900000}
+                renderer={renderer}
+            />
+        </div>
+    );
+
+
+    function getBgStyle() {
+        let bgWidth = getBgWidth();
+        let bgHeight = getBgHeight(bgWidth);
+
+        return {
+            width: `${bgWidth}px`,
+            height: `750px`,
+            backgroundSize: `${bgWidth}px ${bgHeight}px`,
+        }
+    }
+
+    function getBgWidth() {
+        return Math.min(windowWidth, 900).toString();
+    }
+    function getBgHeight(width) {
+        return (16 * width) / 9;
+    }
 }
 
 export default App;
