6d26a45b910467133ef1e0f2efa2e5462022b4d6
ghunley <ghunley.zhang@gmail.com>
2021-07-30 20:55:32 -0700

    Resize font for small screens

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     const [windowHeight, setWindowHeight] = React.useState(window.innerHeight);
 
     // Random component
-    const Completionist = () => <span>You are good to go!</span>;
+    const Completionist = () => <div class="message" style={getMessageStyle()}>Yuxiao's flight has departed'!</div>;
 
     // Renderer callback with condition
     const renderer = ({ days, hours, minutes, seconds, completed }) => {
@@ -32,10 +32,10 @@ function App() {
         return(
             <table class="countdown-container">
                 <tr>
-                    <td class="countdown-number">{days}</td>
-                    <td class="countdown-number">{hours}</td>
-                    <td class="countdown-number">{minutes}</td>
-                    <td class="countdown-number">{seconds}</td>
+                    <td class="countdown-number" style={getCountdownBoxStyle()}>{days}</td>
+                    <td class="countdown-number" style={getCountdownBoxStyle()}>{hours}</td>
+                    <td class="countdown-number" style={getCountdownBoxStyle()}>{minutes}</td>
+                    <td class="countdown-number" style={getCountdownBoxStyle()}>{seconds}</td>
                 </tr>
                 <tr>
                     <td class="countdown-label">days</td>
@@ -51,7 +51,7 @@ function App() {
 
     return (
         <div className="background" style={getBgStyle()}>
-            <div class="message">Yuxiao's flight to Seattle is departing in</div>
+            <div class="message" style={getMessageStyle()}>Yuxiao's flight to Seattle is departing in</div>
             <Countdown
                 date={1629240900000}
                 renderer={renderer}
@@ -59,20 +59,36 @@ function App() {
         </div>
     );
 
+    function getCountdownBoxStyle() {
+        let width = Math.min(150, getBgWidth() / 4);
+        let fontSize = Math.min(100, Math.floor(width * 0.666));
+        return {
+            width: `${width}px`,
+            fontSize: `${fontSize}px`,
+        }
+    }
+
+    function getMessageStyle() {
+        let fontSize = Math.min(50, Math.floor(getBgWidth() / 12))
+        return {
+            fontSize: `${fontSize}px`,
+        }
+    }
 
     function getBgStyle() {
         let bgWidth = getBgWidth();
         let bgHeight = getBgHeight(bgWidth);
+        let height = Math.min(750, windowHeight, bgHeight);
 
         return {
             width: `${bgWidth}px`,
-            height: `750px`,
+            height: `${height}px`,
             backgroundSize: `${bgWidth}px ${bgHeight}px`,
         }
     }
 
     function getBgWidth() {
-        return Math.min(windowWidth, 900).toString();
+        return Math.min(windowWidth, 900)
     }
     function getBgHeight(width) {
         return (16 * width) / 9;
